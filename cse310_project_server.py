1f370655ddd420f3bbb68f56bf6f869bbf83ddb4
aalin <aaron.lin@stonybrook.edu>
2016-12-10 19:06:51 -0500

    Able to send posts from rg command over to client

--- cse310_project_server.py
+++ cse310_project_server.py
@@ -16,7 +16,7 @@ from re import *
 
 # SERVER IS STARTED AND WAITS AT A KNOWN PORT FOR REQUESTS FROM CLIENTS
 # SHOULD IT BE (!TCP!) OR UDP CONNECTION?
-serverPort = 12000 # HARD CODE SERVER PORT
+serverPort = 12001 # HARD CODE SERVER PORT
 serverSocket = socket(AF_INET, SOCK_STREAM) # CREATING SOCKET AND BINDING IT TO PORT
 serverSocket.bind(('', serverPort))
 serverSocket.listen(100)  # ONLY LISTEN TO 100 CLIENTS
@@ -45,7 +45,7 @@ while True:
     print(commands)
 
     if (commands[0] == "ag"):
-        print('ag command entered')
+        print('Entered command state of ag')
         default_display = 5
         try:
             if commands[1] != "":
@@ -62,32 +62,40 @@ while True:
         connectionSocket.sendall(ag_info.encode('UTF-8'))
 
         while (ag_flag == 0):
+            print('Entered subcommands state of ag')
             ag_data = connectionSocket.recv(128)
             ag_subcommands = ag_data.split(" ")
 
             if (ag_subcommands[0] == "n"):
                 print("Command n entered")
-                ag_more_info = ""
-                with open('server.json') as serverfile:
 
-                    server_data = load(serverfile)
-                    read_n = default_display + int(ag_subcommands[1])
-                    if (read_n > numelements):
-                        read_n = numelements
+                try:
+                    ag_more_info = ""
+                    with open('server.json') as serverfile:
 
-                    for i in range(default_display, read_n):
-                        ag_more_info += server_data['discussion_groups'][i]['groupname']
-                        ag_more_info += '\n'
-                connectionSocket.sendall(ag_more_info.encode('UTF-8'))
+                        server_data = load(serverfile)
+                        read_n = default_display + int(ag_subcommands[1])
+                        if (read_n > numelements):
+                            read_n = numelements
+
+                        for i in range(default_display, read_n):
+                            ag_more_info += server_data['discussion_groups'][i]['groupname']
+                            ag_more_info += '\n'
+                    connectionSocket.sendall(ag_more_info.encode('UTF-8'))
+                except IndexError:
+                    connectionSocket.sendall("Please enter a number for subcommand 'n' ")
+                    continue
 
             elif (ag_subcommands[0] == "q"):
                 print("Command q entered")
                 print("Closed out of ag command")
                 connectionSocket.sendall("AG 500 CLOSE")
                 break
+            else:
+                break
 
     elif (commands[0] == "sg"):
-        print('sg comamnd entered')
+        print('Entered command state of sg')
         display = 5
         try:
             if not commands[1]:
@@ -109,32 +117,62 @@ while True:
 
             if (sg_subcommands[0] == "q"):
                 print("Command q entered")
-                print("Closed out of ag command")
+                print("Closed out of sg command")
                 connectionSocket.sendall("SG 500 CLOSE")
                 break
 
 
     elif (commands[0] == "rg"):
+        print('Entered command state of rg')
+
+        gname = ""
         display = 5
         try:
-            if not commands[2]:
-                display = commands[2]
+            if commands[1] != "":
+                gname = commands[1]
         except IndexError:
             pass
         try:
-            if not commands[1]:
-                gname = commands[1]
+            if commands[2] != "":
+                display = commands[2]
         except IndexError:
             connectionSocket.send("Require a mandatory argument 'gname'\n")
-            continue
-            
-        connectionSocket.send("1. N  Nov 12 19:32:02   Sort a Python dictionary by value \n" +
-                              "2. N  Nov 11 08:11:34   How to print to stderr in Python? \n" +
-                              "3. N  Nov 10 22:05:47   \"Print\" and \"Input\" in one file \n" +
-                              "4.    Nov  9 13:59:05   How not to display the user inputs? \n" +
-                              "5.    Nov  9 12:46:10   Declaring custom exceptions \n")
+            break
+        
+        postnum = 0
+        rg_info = ""
+        with open('server.json') as serverfile:
+            server_data = load(serverfile)
+            for i in range(numelements):
+                if (server_data['discussion_groups'][i]['groupname'] == gname):
+                    for j in server_data['discussion_groups'][i]['posts']:
+                        postnum += 1
+                    for k in range(postnum):
+                        rg_info += server_data['discussion_groups'][i]['posts'][k]['postid']
+                        rg_info += ','
+                        rg_info += server_data['discussion_groups'][i]['posts'][k]['subject_line']
+                        rg_info += ','
+                        rg_info += server_data['discussion_groups'][i]['posts'][k]['content_body']
+                        rg_info += ','
+                        rg_info += server_data['discussion_groups'][i]['posts'][k]['author_id']
+                        rg_info += ','
+                        rg_info += server_data['discussion_groups'][i]['posts'][k]['timestamp']
+                        rg_info += '\n'
 
-        continue                
+
+        connectionSocket.sendall(rg_info.encode('UTF-8'))
+
+        while (rg_flag == 0):
+            rg_data = connectionSocket.recv(128)
+            rg_subcommands = ag_data.split(" ")
+
+            if (rg_subcommands[0] == "q"):
+                print("Command q entered")
+                print("Closed out of rg command")
+                connectionSocket.sendall("RG 500 CLOSE")
+                break
+
+                        
     elif (commands[0] == "logout"):
         connectionSocket.send('Closing your connection\r\n')
         print('Closing connection to client\n')
